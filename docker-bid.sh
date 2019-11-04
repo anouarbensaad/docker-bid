90a75734ced89d171c4a89de42a4eaceb2e0e31e
Anouar Ben Saad <bensaad.tig@gmail.com>
2019-11-04 19:52:26 +0100

    Update docker-bid.sh

--- docker-bid.sh
+++ docker-bid.sh
@@ -1,45 +1,47 @@
 # Init
-LESSKEY=false
+lesskey=false
 tput civis   
-CURRENT_POS=1         
-INDEX=1
-END=false
-EXT=0
+current_position=1         
+idx=1
+end=false
+ext=0
 
-RED=`echo -e '\033[1;31m'`
-GREEN=`echo -e '\033[1;32m'`
-YELLOW=`echo -e '\033[1;33m'`
-NORMAL=`echo -e '\033[0m'`
-WHITEBOLD_RE=`echo -e '\033[1m'`
-WHITE_BOLD="\033[1m"
+red=`echo -e '\033[1;31m'`
+green=`echo -e '\033[1;32m'`
+yellow=`echo -e '\033[1;33m'`
+normal=`echo -e '\033[0m'`
+whitebold_re=`echo -e '\033[1m'`
+whitebold="\033[1m"
 OFF="\033[0m"
 
 # setkeys.
-ArrowUp="`echo -e '\e[A'`" 
-ArrowDown="`echo -e '\e[B'`" 
+arrowup="`echo -e '\e[A'`" 
+arrowdown="`echo -e '\e[B'`" 
+arrowright="`echo -e '\e[B'`" 
+arrowleft="`echo -e '\e[B'`" 
 #escape | echape
 ec="`echo -e '\e'`"
 #entré
 nl="`echo -e '\n'`"
-ArrowUp_1="k"
-ArrowDown_1="j"
+arrowup_1="k"
+arrowdown_1="j"
 nl_1="e"
 
 
-function tputcolors() {
-    NORMAL=$(tput sgr0)
-    REVERSE=$(tput rev)
+tputcolors() {
+    normal=$(tput sgr0)
+    reverse=$(tput rev)
     BLUE=$(tput setaf 6)
 } 
 
-function untputcolors() {
-    GREEN=""
-    YELLOW=""
-    NORMAL=""
-    REVERSE=""
+untputcolors() {
+    green=""
+    yellow=""
+    normal=""
+    reverse=""
 }
 
-function cls() {
+cls() {
     echo -e "\E[J"
     tput cuu1
 }
@@ -55,76 +57,76 @@ if [[ -t 1 ]] && which tput &>/dev/null && tput colors &>/dev/null; then
 else
     untputcolors
 fi
-DOCKER_LINES=$(docker container list -a|wc -l)
-DOCKER_LIST_VIEW=$(($DOCKER_LINES - 1))
+docker_lines=$(docker container list -a|wc -l)
+docker_listview=$(($docker_lines - 1))
 
 # docker view template.
-DOCKER_SET="docker container list -a \
+docker_container_list="docker container list -a \
             --format \"table STATUSCOLOR {{.ID}}OFFCOLOR − {{.Image}}\tBOLDCOLOR({{.RunningFor}})OFFCOLOR\t{{.Status}}\tEXPOSECOLOR{{.Ports}}OFFCOLOR\tBOLDCOLOR<{{.Names}}>OFFCOLOR\" |\
-            tail -$DOCKER_LIST_VIEW |\
-            sed -e \"s/OFFCOLOR/$NORMAL/g\" \
-            -e \"/Exited/s/STATUSCOLOR/$RED/\" \
-            -e \"/Up/s/STATUSCOLOR/$GREEN/\" \
-            -e \"/Created/s/STATUSCOLOR/$YELLOW/\" \
+            tail -$docker_listview |\
+            sed -e \"s/OFFCOLOR/$normal/g\" \
+            -e \"/Exited/s/STATUSCOLOR/$red/\" \
+            -e \"/Up/s/STATUSCOLOR/$green/\" \
+            -e \"/Created/s/STATUSCOLOR/$yellow/\" \
             -e \"s/EXPOSECOLOR/$BLUE/g\" \
-            -e \"s/BOLDCOLOR/$WHITEBOLD_RE/g\" "
+            -e \"s/BOLDCOLOR/$whitebold_re/g\" "
 
 
 
-RUNNING_CONTAINER=$(docker container list -a |grep Up|wc -l)
-EXITED_CONTAINER=$(docker container list -a |grep Exited|wc -l)
-CREATED_CONTAINER=$(docker container list -a |grep Created|wc -l)
+running_container=$(docker container list -a |grep Up|wc -l)
+exited_container=$(docker container list -a |grep Exited|wc -l)
+created_container=$(docker container list -a |grep Created|wc -l)
 
 IFS=$'\n'
-DOCKER=($(eval ${DOCKER_SET} 2>/dev/null))
+docker=($(eval ${docker_container_list} 2>/dev/null))
 unset IFS
 
 NumberC=$(docker container ls -a |wc -l)
-CONTAINER_INDEX=${#DOCKER[@]}
+container_idx=${#docker[@]}
 
 SN=$(( `tput lines` - 1 ))
 CN=$(tput cols)
-LINE_USED=$(( $CONTAINER_INDEX < $((SN -1)) ? $CONTAINER_INDEX : $((SN -1))))
-OFFSET=0 
+line_used=$(( $container_idx < $((SN -1)) ? $container_idx : $((SN -1))))
+offset=0 
 
-echo -e "containers:${WHITE_BOLD} $LINE_USED ${OFF}, running:${WHITE_BOLD} $RUNNING_CONTAINER${OFF}, exited:${WHITE_BOLD} $EXITED_CONTAINER ${OFF}, created:${WHITE_BOLD} $CREATED_CONTAINER ${OFF}\n"
-echo -e "$WHITE_BOLD −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−\n \
-CONTAINER ID − IMAGE              (CREATED)        STATUS                           PORTS                    <NAMES>\n \
+echo -e "containers:${whitebold} $line_used ${OFF}, running:${whitebold} $running_container${OFF}, Exited:${whitebold} $exited_container ${OFF}, created:${whitebold} $created_container ${OFF}\n"
+echo -e "$whitebold −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−\n \
+container ID − IMAGE              (CREATED)        STATUS                           PORTS                    <NAMES>\n \
 −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−"
 
-if [[ $DOCKER_LIST_VIEW -eq 0 ]];then
+if [[ $docker_listview -eq 0 ]];then
     exit 0;
 fi
 
 if command -v lesskey &> /dev/null; then
-    LESSKEY=true
+    lesskey=true
 fi
-case "$FILTRED" in
-    *)       SED_CMD="sed -r s/^\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" ;;
+case "$filtred" in
+    *)       regex="sed -r s/^\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" ;;
 esac
 
-for elt in "${DOCKER[@]}"
+for elt in "${docker[@]}"
 do
     
-    ELT="$(echo "$elt" | $SED_CMD)"
-    CONTAINER_UNCOL+=("$ELT")
+    ELT="$(echo "$elt" | $regex)"
+    container_uncoll+=("$ELT")
 done
 
-for C in "${CONTAINER_UNCOL[@]}"
+for C in "${container_uncoll[@]}"
 do
     if [[ ${#C} -gt $CN ]]; then
-	OFFSET=$(( OFFSET + 1 ))
+	offset=$(( offset + 1 ))
     fi
 done
 
-if [[ $LESSKEY = true ]]; then
+if [[ $lesskey = true ]]; then
     echo "\t quit" | lesskey -o /tmp/lsh_less_keys_tmp -- - &> /dev/null
 fi
-function get_containers() {
-    ELT="$(echo "${CONTAINER_UNCOL[$CURRENT_POS-1]}")"
+get_containers() {
+    ELT="$(echo "${container_uncoll[$current_position-1]}")"
     #extract hash_container started from 8 - 10 characters
-    CONTAINER_HASH=${ELT:8:10}
-#    INSPECT_DOCKER="docker container inspect $CONTAINER_HASH"
+    container_HASH=${ELT:8:10}
+#    inspect_docker="docker container inspect $container_HASH"
 #                    --format \"table IpAdress   : {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}\n      \
 #                               MacAdress  : {{range .NetworkSettings.Networks}}{{.MacAddress}}{{end}} \n    \
 #                               PathLog    : {{.LogPath}}\n                                                  \
@@ -132,34 +134,34 @@ function get_containers() {
 #                               StartedAt  : {{.State.StartedAt}}\n                                          \
 #                               FinishedAt : {{.State.FinishedAt}}\n                                         \
 #                               Status     : {{.State.Status}}\" "
-    INSPECT_DOCKER="docker container diff $CONTAINER_HASH"
-    CONTAINER=$(eval ${INSPECT_DOCKER} 2>/dev/null)
-    tmp_diff="$(echo "$CONTAINER" | $SED_CMD)"
+    inspect_docker="docker container diff $container_HASH"
+    container=$(eval ${inspect_docker} 2>/dev/null)
+    tmp_diff="$(echo "$container" | $regex)"
     off=$(echo "$tmp_diff" | grep -c ".\{$CN\}")
-    DIFF_LINES_NUMBER="$(echo "$CONTAINER" | wc -l)"
-    DIFF_LINES_NUMBER=$(( DIFF_LINES_NUMBER + off ))
+    inspect_line_numbers="$(echo "$container" | wc -l)"
+    inspect_line_numbers=$(( inspect_line_numbers + off ))
 }
 
-function set_containers() {
+set_containers() {
     get_containers
-    if [[ $(( LINE_USED + DIFF_LINES_NUMBER + OFFSET )) -ge $(( `tput lines` - 1 )) ]]; then
+    if [[ $(( line_used + inspect_line_numbers + offset )) -ge $(( `tput lines` - 1 )) ]]; then
 	trap - INT
-	if [[ $LESSKEY = true ]]; then
-	    echo "$CONTAINER" | less -r -k /tmp/lsh_less_keys_tmp
+	if [[ $lesskey = true ]]; then
+	    echo "$container" | less -r -k /tmp/lsh_less_keys_tmp
 	else 
-	    echo "$CONTAINER" | less -r
+	    echo "$container" | less -r
 	fi
 	trap cleanup INT
 	cls
     else 
 	stop=false
 	cls
-	for i in `seq 1 $LINE_USED`
+	for i in `seq 1 $line_used`
 	do
-	    echo -n "$NORMAL"
-	    [[ $CURRENT_POS == "$i" ]] && echo -n "$REVERSE"
-	    echo "${DOCKER[$i - 1]}"
-	    [[ $CURRENT_POS == "$i" ]] && echo "$CONTAINER"
+	    echo -n "$normal"
+	    [[ $current_position == "$i" ]] && echo -n "$reverse"
+	    echo "${docker[$i - 1]}"
+	    [[ $current_position == "$i" ]] && echo "$container"
 	done
 	while ! $stop
 	do
@@ -170,34 +172,34 @@ function set_containers() {
 		    ;;
 		"q")
 		    stop=true
-		    END=true
+		    end=true
 		    ;;
 	    esac
 	done
-	[[ $END = false ]] && tput cuu $(( LINE_USED + DIFF_LINES_NUMBER + OFFSET )) && cls
+	[[ $end = false ]] && tput cuu $(( line_used + inspect_line_numbers + offset )) && cls
     fi
 }
 
-function calculate_offset {
+calculate_offset {
     tmp=1
-    index=$(( INDEX -1 ))
+    idx=$(( idx -1 ))
     while [[ $tmp -lt $SN ]]
     do
-    	el=${CONTAINER_UNCOL[$index]}
-    	if [[ ${#el} -gt $CN ]] && [[ $CURRENT_POS -lt $((SN -1)) ]]; then
-    	    OFFSET_2=$(( OFFSET_2 + 1 ))
+    	el=${container_uncoll[$idx]}
+    	if [[ ${#el} -gt $CN ]] && [[ $current_position -lt $((SN -1)) ]]; then
+    	    offset_2=$(( offset_2 + 1 ))
     	    tmp=$(( tmp + 1 ))
     	fi
     	tmp=$(( tmp + 1 ))
-    	index=$(( index + 1 ))
+    	idx=$(( idx + 1 ))
     done
 }
-function cleanup() {
-    [[ $LESSKEY = true ]] && rm /tmp/lsh_less_keys_tmp
+cleanup() {
+    [[ $lesskey = true ]] && rm /tmp/lsh_less_keys_tmp
 
     tput cuu 1
     tput cnorm
-    echo "$NORMAL"
+    echo "$normal"
     stty "$orig_stty" > /dev/null 2>&1
     exit
 }
@@ -207,26 +209,26 @@ trap cleanup INT
 {
 
 
-while ! $END
+while ! $end
 do
-    END_INDEX=0
-    if [[  $LINE_USED == $CONTAINER_INDEX ]]; then 
-	END_INDEX=$LINE_USED
-	OFFSET_2=$OFFSET
-    elif [[  $LINE_USED == $(( SN - 1 )) ]]; then
-	if [[ $OFFSET != 0 ]]; then 
-   	    [[ $CURRENT_POS -lt $((SN -1)) ]] && OFFSET_2=0
-   	    EXT=1
+    end_idx=0
+    if [[  $line_used == $container_idx ]]; then 
+	end_idx=$line_used
+	offset_2=$offset
+    elif [[  $line_used == $(( SN - 1 )) ]]; then
+	if [[ $offset != 0 ]]; then 
+   	    [[ $current_position -lt $((SN -1)) ]] && offset_2=0
+   	    ext=1
    	    calculate_offset
 	fi
-	END_INDEX=$(( LINE_USED + INDEX -1 + EXT - OFFSET_2 ))
+	end_idx=$(( line_used + idx -1 + ext - offset_2 ))
     fi
 
-    for i in `seq $INDEX $END_INDEX`
+    for i in `seq $idx $end_idx`
     do
-	echo -n "$NORMAL"
-	[[ $CURRENT_POS == $i ]] && echo -n "$REVERSE"
-	echo "${DOCKER[$i - 1]}"
+	echo -n "$normal"
+	[[ $current_position == $i ]] && echo -n "$reverse"
+	echo "${docker[$i - 1]}"
     done
 
     read -sn 1 key
@@ -237,37 +239,37 @@ do
     }
 
     case "$key" in
-	"$ArrowUp" | "$ArrowUp_1")
-            CURRENT_POS=$(( CURRENT_POS - 1 ))
-            [[ $CURRENT_POS == 0 ]] && [[ $INDEX == 1 ]] && [[ $LINE_USED == $(( SN - 1 )) ]] && CURRENT_POS=$CONTAINER_INDEX && INDEX=$(( CONTAINER_INDEX - SN + 2 + OFFSET_2 ))
-            [[ $CURRENT_POS == 0 ]] && [[ $INDEX == 1 ]] && [[ $LINE_USED == $CONTAINER_INDEX ]] && CURRENT_POS=$LINE_USED
-            [[ $CURRENT_POS == $(( INDEX - 1 )) ]] && [[ $INDEX != 1 ]] && INDEX=$(( INDEX - 1 ))
+	"$arrowup" | "$arrowup_1")
+            current_position=$(( current_position - 1 ))
+            [[ $current_position == 0 ]] && [[ $idx == 1 ]] && [[ $line_used == $(( SN - 1 )) ]] && current_position=$container_idx && idx=$(( container_idx - SN + 2 + offset_2 ))
+            [[ $current_position == 0 ]] && [[ $idx == 1 ]] && [[ $line_used == $container_idx ]] && current_position=$line_used
+            [[ $current_position == $(( idx - 1 )) ]] && [[ $idx != 1 ]] && idx=$(( idx - 1 ))
 
-   	    [[ $LINE_USED != $(( SN - 1 )) ]] && tput cuu $(( LINE_USED + OFFSET_2 ))
-   	    [[ $LINE_USED == $(( SN - 1 )) ]] && tput cuu $(( LINE_USED + EXT ))
-            [[ $INDEX != 1 ]] && cls
+   	    [[ $line_used != $(( SN - 1 )) ]] && tput cuu $(( line_used + offset_2 ))
+   	    [[ $line_used == $(( SN - 1 )) ]] && tput cuu $(( line_used + ext ))
+            [[ $idx != 1 ]] && cls
             ;;
-	"$ArrowDown" | "$ArrowDown_1")
-   	    CURRENT_POS=$(( CURRENT_POS + 1 ))
-            [[ $CURRENT_POS == $(( CONTAINER_INDEX + 1 )) ]] && CURRENT_POS=1 && INDEX=1 
-            [[ $CURRENT_POS == $(( SN + INDEX - 1 + EXT - OFFSET_2 )) ]] && [[ $LINE_USED == $(( SN - 1 )) ]] && INDEX=$(( INDEX + 1 ))
+	"$arrowdown" | "$arrowdown_1")
+   	    current_position=$(( current_position + 1 ))
+            [[ $current_position == $(( container_idx + 1 )) ]] && current_position=1 && idx=1 
+            [[ $current_position == $(( SN + idx - 1 + ext - offset_2 )) ]] && [[ $line_used == $(( SN - 1 )) ]] && idx=$(( idx + 1 ))
 
-   	    [[ $LINE_USED != $(( SN - 1 )) ]] && tput cuu $(( LINE_USED + OFFSET_2 ))
-   	    [[ $LINE_USED == $(( SN - 1 )) ]] && tput cuu $(( LINE_USED + EXT ))
-            [[ $INDEX != 1 ]] && cls
-            [[ $INDEX = 1 ]] && [[ $CURRENT_POS = 1 ]] && cls
+   	    [[ $line_used != $(( SN - 1 )) ]] && tput cuu $(( line_used + offset_2 ))
+   	    [[ $line_used == $(( SN - 1 )) ]] && tput cuu $(( line_used + ext ))
+            [[ $idx != 1 ]] && cls
+            [[ $idx = 1 ]] && [[ $current_position = 1 ]] && cls
             ;;
 	"$nl" | "$nl_1")
-   	    [[  $LINE_USED == $CONTAINER_INDEX ]] && tput cuu $(( LINE_USED + OFFSET_2 ))
-   	    [[  $LINE_USED != $CONTAINER_INDEX ]] && tput cuu $(( LINE_USED + EXT ))
+   	    [[  $line_used == $container_idx ]] && tput cuu $(( line_used + offset_2 ))
+   	    [[  $line_used != $container_idx ]] && tput cuu $(( line_used + ext ))
             set_containers 
             ;;
 	"q")
-            INDEX=false
-            END=true
+            idx=false
+            end=true
             ;;
 	* )
-   	    tput cuu $(( LINE_USED + OFFSET_2 ))
+   	    tput cuu $(( line_used + offset_2 ))
     esac
 done
 
